1cdbe662177983b23cb56d59bb060e0fb173cebf
bnuq <31835672+bnuq@users.noreply.github.com>
2022-03-23 15:57:45 +0900

    Add library and dll

---
+++ 1-Introduction/3 Setting Up.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//OpenGL 사용을 위해서, 외부에서 추가한 라이브러리
+#include <GL/glew.h>	//GLEW
+#include <GLFW/glfw3.h> //GLFW
+
+//Window Dimensions, by constant
+const GLint WIDTH{ 800 }, HEIGHT{ 600 };
+
+int main() {
+
+
+	return 0;
+}
